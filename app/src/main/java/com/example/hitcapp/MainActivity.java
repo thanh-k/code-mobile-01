293fc8302c1b774c71046e03c69ea33ad1248e8c
thanh-k <kieuxuanthanh999@gmail.com>
2025-08-07 11:20:49 +0700

    làm giao diện trang chủ (main), call API login

--- app/src/main/java/com/example/hitcapp/MainActivity.java
+++ app/src/main/java/com/example/hitcapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.hitcapp;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +14,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+public class MainActivity extends AppCompatActivity {
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "https://68940ddebe3700414e11df37.mockapi.io/log/user";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +34,28 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
-        Intent intent = getIntent();
-        String txtPhone = intent.getStringExtra("phone");
-        String txtpass = intent.getStringExtra("pass");
+        getData();
+    }
+    private void getData() {
+        // RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
 
-        TextView tvWelcome = findViewById(R.id.tvWelcome);
+        // String Request initialized
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
 
-        // Hiển thị lời chào
-        tvWelcome.setText("Xin chào " + txtPhone);
+                Toast.makeText(getApplicationContext(), "Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
     }
 }
--- app/src/main/java/com/example/hitcapp/login.java
+++ app/src/main/java/com/example/hitcapp/login.java
@@ -13,49 +13,110 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class login extends AppCompatActivity {
+
+    EditText editPhone, editPass;
+    String url = "https://68940ddebe3700414e11df37.mockapi.io/log/user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btn = findViewById(R.id.btnLogin);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        editPhone = findViewById(R.id.etPhone);
+        editPass = findViewById(R.id.etPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText editPhone = findViewById(R.id.etPhone);
-                String txtPhone = editPhone.getText().toString();
+                String inputPhone = editPhone.getText().toString().trim();
+                String inputPass = editPass.getText().toString().trim();
 
-                EditText editPass = findViewById(R.id.etPassword);
-                String txtPass = editPass.getText().toString();
-
-                CharSequence text = txtPhone + " " + txtPass;
-                int duration = Toast.LENGTH_SHORT;
-
-                if (txtPhone.equals("090") && txtPass.equals("123")) {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    intent.putExtra("phone", txtPhone);
-                    intent.putExtra("pass", txtPass);
-                    startActivity(intent);
-                } else {
-                    Toast toast = Toast.makeText(getApplicationContext(), "Login fail", duration);
-                    toast.show();
+                if (inputPhone.isEmpty() || inputPass.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+
+                checkLogin(inputPhone, inputPass);
             }
         });
-        Button btn2 = findViewById(R.id.btnRegister);
-        btn2.setOnClickListener(new View.OnClickListener() {
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent it = new Intent(getApplicationContext(), register.class);
                 startActivity(it);
             }
         });
+    }
+
+    private void checkLogin(String phone, String pass) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        boolean isLoginSuccess = false;
+
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject user = response.getJSONObject(i);
+                                String apiPhone = user.getString("phone");
+                                String apiPass = user.getString("pass");
+                                String name = user.getString("name");
+
+                                if (phone.equals(apiPhone) && pass.equals(apiPass)) {
+                                    isLoginSuccess = true;
+                                    break;
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        if (isLoginSuccess) {
+                            Toast.makeText(getApplicationContext(), "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.putExtra("phone", phone);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Sai số điện thoại hoặc mật khẩu", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Lỗi kết nối đến server", Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                }
+        );
+
+        queue.add(request);
     }
 }
